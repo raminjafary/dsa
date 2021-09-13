ab494e85dfe826dd73287827e73cbc3ac976f4e2
raminjafary <raminjafary1993@gmail.com>
2021-09-14 00:10:48 +0430

    feat(go/data-structure): implement queue

--- go/data-structure/main.go
+++ go/data-structure/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	q "ds/queue"
 	s "ds/stack"
 	"fmt"
 )
 
 func main() {
 	runStack()
+	runQueue()
 }
 
 func runStack() {
@@ -19,4 +21,16 @@ func runStack() {
 	stack.Pop()
 	fmt.Println(stack.Peek())
 	fmt.Printf("stack length is %v and the capacity %v \n", len(stack), cap(stack))
+}
+
+func runQueue() {
+	var queue q.Queue
+
+	queue.Enqueue(5)
+	queue.Enqueue(6)
+	queue.Enqueue(2)
+	queue.Enqueue("hello")
+	queue.Dequeue()
+	fmt.Println(queue.Peek())
+	fmt.Printf("queue length is %v with the capacity of %v \n", len(queue), cap(queue))
 }
