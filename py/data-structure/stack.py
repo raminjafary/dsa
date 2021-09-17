aef9dfb6a864e68e90d42c20cf5eda8ac4969e31
raminjafary <raminjafary1993@gmail.com>
2021-09-17 20:04:20 +0430

    refactor(py): add types to stack module

--- py/data-structure/stack.py
+++ py/data-structure/stack.py
@@ -1,27 +1,33 @@
-class Stack:
-    stack = []
+from typing import Union, List, TypeVar
 
-    def push(self, value):
+T = TypeVar("T", str, int)
+Vector = List[T]
+
+
+class Stack:
+    stack: Vector = []
+
+    def push(self, value: T) -> None:
         self.stack.append(value)
 
-    def pop(self):
+    def pop(self) -> Union[T, bool]:
         if(self.is_empty()):
-            return
-        self.stack.pop()
+            return False
+        return self.stack.pop()
 
-    def is_empty(self):
+    def is_empty(self) -> bool:
         return not len(self.stack)
 
-    def peek(self):
+    def peek(self) -> Union[T, bool]:
         if(self.is_empty()):
-            return
+            return False
         return self.stack[len(self.stack) - 1]
 
 
 s = Stack()
 s.push("value")
 s.push(3)
-s.push([1, 5])
+s.push(8)
 print("the last item is ", s.peek())
 print("stack -> {}".format(s.stack))
 print("stack length is:", len(s.stack))
