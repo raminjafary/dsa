0eeab3d8ba7a769cd4c6372b7a781d8fdf3814ee
raminjafary <raminjafary1993@gmail.com>
2021-09-19 00:00:21 +0430

    feat(js/algorithm): implement bubble sort

---
+++ js/algorithm/bubble-sort.js
@@ -0,0 +1,23 @@
+
+let swapped = true;
+
+
+function bubbleSort(items) {
+    while (swapped) {
+
+        swapped = false
+
+        for (let i = 0; i < items.length - 1; i++) {
+
+            if (items[i] > items[i + 1]) {
+                [items[i], items[i + 1]] = [items[i + 1], items[i]]
+                swapped = true
+            }
+        }
+    }
+}
+
+
+const items = [0, 5, -2, 10, 3, 5]
+bubbleSort(items)
+console.log(items);
