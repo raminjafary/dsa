153062dce3c1c3c16e24dcf3382c2c5724b5f80a
raminjafary <raminjafary1993@gmail.com>
2021-09-27 22:01:51 +0330

    feat(go/algorithm): implement insertion sort

--- go/algorithm/main.go
+++ go/algorithm/main.go
@@ -2,16 +2,25 @@ package main
 
 import (
 	"algorithm/bubble_sort"
+	"algorithm/insertion_sort"
 	"fmt"
 )
 
 func main() {
 	bubbleSort()
+	insertionSort()
 }
 
 func bubbleSort() {
 	vec := []int{1, 8, 6, 0, 2, 4, 7, -1, 10}
 
 	bubble_sort.Sort(&vec)
-	fmt.Println(vec)
+	fmt.Printf("bubble sort %v\n", vec)
+}
+
+func insertionSort() {
+	items := []int{5, 1, 0, -1, 20, 50}
+
+	insertion_sort.Sort(items)
+	fmt.Printf("insertion sort %v\n", items)
 }
