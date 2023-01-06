9136f47b5460270f6a7ad35e3d263551599bef63
raminjafary <raminjafary1993@gmail.com>
2023-01-06 23:02:08 +0330

    implement stack

--- js/data-structure/stack.js
+++ js/data-structure/stack.js
@@ -1,31 +1,23 @@
-function stack() {
-  const prop = stack.prototype;
-  prop.stack = [];
-  return prop;
-}
+const SinglyLinkedList = require("./singlyLinkedList");
 
-stack.prototype.push = function (val) {
-  this.stack.push(val);
-};
-
-stack.prototype.isEmpty = function () {
-  return !this.stack.length;
-};
-
-stack.prototype.peek = function () {
-  return this.stack[this.stack.length - 1];
-};
-
-stack.prototype.pop = function () {
-  if (this.isEmpty()) {
-    return false;
+class Stack {
+  constructor() {
+    this.stack = new SinglyLinkedList();
   }
-  return this.stack.pop();
-};
 
-const s = stack();
-s.push(5);
-s.push(56);
-s.push(59);
-s.pop();
-console.log(s.peek());
+  push(value) {
+    this.stack.unshift(value);
+  }
+
+  isEmpty() {
+    return this.stack.length === 0;
+  }
+
+  peek() {
+    return this.stack.head;
+  }
+
+  pop() {
+    return this.stack.shift();
+  }
+}
