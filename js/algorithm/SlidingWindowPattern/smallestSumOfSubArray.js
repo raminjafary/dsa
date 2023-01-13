d27d5915ac9e8da4ef5f26b95bfce734553f97b7
raminjafary <raminjafary1993@gmail.com>
2023-01-14 00:45:35 +0330

    implement smallest subarray with a given sum

--- js/algorithm/SlidingWindowPattern/smallestSumOfSubArray.js
+++ js/algorithm/SlidingWindowPattern/smallestSumOfSubArray.js
@@ -1,23 +1,39 @@
-function smallestSumOfSubArray(sum, arr) {
+// Smallest Subarray with a given sum
+
+// Given an array of positive numbers and a positive number ‘n’,
+// find the length of the smallest contiguous subarray whose sum is greater than or equal to ‘n’. Return 0, if no such subarray exists.
+
+// Input: [2, 1, 5, 2, 3, 2], n=7
+// Output: 2
+// Explanation: The smallest subarray with a sum great than or equal to '7' is [5, 2].
+
+// Input: [2, 1, 5, 2, 8], n=7
+// Output: 1
+// Explanation: The smallest subarray with a sum greater than or equal to '7' is [8].
+
+// Input: [3, 4, 1, 1, 6], n=8
+// Output: 3
+// Explanation: Smallest subarrays with a sum greater than or equal to '8' are [3, 4, 1] or [1, 1, 6].
+
+function smallestSumOfSubArray(n, arr) {
+  let minLength = Infinity;
   let windowStart = 0;
   let windowSum = 0;
-  let smallestLength = Infinity;
 
   for (let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
     windowSum += arr[windowEnd];
 
-    while (windowSum >= sum) {
-      smallestLength = Math.min(smallestLength, windowEnd - windowStart + 1);
+    while (windowSum >= n) {
+      minLength = Math.min(minLength, windowEnd - windowStart + 1);
       windowSum -= arr[windowStart];
       windowStart += 1;
     }
   }
-
-  if (smallestLength === Infinity) {
+  if (minLength === Infinity) {
     return 0;
   }
 
-  return smallestLength;
+  return minLength;
 }
 
-console.log(smallestSumOfSubArray(7, [2, 1, 5, 2, 3, 2])); // Output: 2 , The smallest subarray with a sum greater than or equal to '7' is [5, 2].
+console.log(smallestSumOfSubArray(7, [2, 1, 5, 2, 3, 2]));
