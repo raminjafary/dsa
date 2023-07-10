ec78e37c4e0b1b3f4ca0d3e9859217822a843f15
raminjafary <raminjafary1993@gmail.com>
2023-07-10 23:28:55 +0330

    feat(js/algorithm): implement Kth Smallest Number in a Sorted Matrix

---
+++ js/algorithm/MergeKSortedList/KthSmallestNumberInSortedMatrix.js
@@ -0,0 +1,51 @@
+const MinHeap = require("../../data-structure/heap");
+
+// Kth Smallest Number in a Sorted Matrix
+
+// Given an  N * N matrix where each row and column is sorted in ascending order, find the Kth smallest element in the matrix.
+
+// Example 1:
+// Input: Matrix=[
+//     [2, 6, 8],
+//     [3, 7, 10],
+//     [5, 8, 11]
+//   ],
+//   K=5
+// Output: 7
+// Explanation: The 5th smallest number in the matrix is 7.
+
+function findKthSmallestNumberInSortedMatrix(matrix, k) {
+  const minHeap = new MinHeap((a, b) => a[0] < b[0]);
+
+  for (let i = 0; i < Math.min(k, matrix.length); i++) {
+    minHeap.insert([matrix[i][0], 0, matrix[i]]);
+  }
+
+  let numberCount = 0;
+  let number = 0;
+
+  while (minHeap.length) {
+    [number, i, row] = minHeap.delete();
+
+    numberCount++;
+
+    if (numberCount === k) break;
+
+    if (row.length > i + 1) {
+      minHeap.insert([row[i + 1], i + 1, row]);
+    }
+  }
+
+  return number;
+}
+
+console.log(
+  findKthSmallestNumberInSortedMatrix(
+    [
+      [2, 6, 8],
+      [3, 7, 10],
+      [5, 8, 11],
+    ],
+    5
+  )
+);
